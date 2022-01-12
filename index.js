49040bdb0984ea06da5b66d058a0abfa50c556f4
Daniel Zhang <dzhangwh@gmail.com>
2022-01-12 10:17:25 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 8
+let num2 = 7.75
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 2) + 1
+
+let num3 = 28
+let num4 = 8
+
+let mod = num3 % num4
+
+let max = Math.max (0, 5, 10, 15, 20)
